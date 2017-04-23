0c8d826230491ef09ed4d208e9bf3c21f8a246e6
stevenkho <stevenkho62@gmail.com>
2017-04-23 15:18:59 +0700

    User Add Back Button dan Skin

--- Login/Login/User.Designer.vb
+++ Login/Login/User.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class User
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -28,16 +28,18 @@ Partial Class User
         Me.username = New MaterialSkin.Controls.MaterialSingleLineTextField()
         Me.password = New MaterialSkin.Controls.MaterialSingleLineTextField()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MaterialRaisedButton2 = New MaterialSkin.Controls.MaterialRaisedButton()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MaterialLabel1
         '
         Me.MaterialLabel1.AutoSize = True
+        Me.MaterialLabel1.BackColor = System.Drawing.Color.White
         Me.MaterialLabel1.Depth = 0
         Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel1.Location = New System.Drawing.Point(12, 13)
+        Me.MaterialLabel1.Location = New System.Drawing.Point(12, 75)
         Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel1.Name = "MaterialLabel1"
         Me.MaterialLabel1.Size = New System.Drawing.Size(118, 19)
@@ -47,10 +49,11 @@ Partial Class User
         'MaterialLabel2
         '
         Me.MaterialLabel2.AutoSize = True
+        Me.MaterialLabel2.BackColor = System.Drawing.Color.White
         Me.MaterialLabel2.Depth = 0
         Me.MaterialLabel2.Font = New System.Drawing.Font("Roboto", 11.0!)
         Me.MaterialLabel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.MaterialLabel2.Location = New System.Drawing.Point(12, 47)
+        Me.MaterialLabel2.Location = New System.Drawing.Point(12, 117)
         Me.MaterialLabel2.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialLabel2.Name = "MaterialLabel2"
         Me.MaterialLabel2.Size = New System.Drawing.Size(80, 19)
@@ -59,44 +62,53 @@ Partial Class User
         '
         'MaterialRaisedButton1
         '
+        Me.MaterialRaisedButton1.AutoSize = True
+        Me.MaterialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MaterialRaisedButton1.Depth = 0
-        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(157, 79)
+        Me.MaterialRaisedButton1.Icon = Nothing
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(443, 152)
         Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
         Me.MaterialRaisedButton1.Primary = True
-        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(114, 23)
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(115, 36)
         Me.MaterialRaisedButton1.TabIndex = 2
         Me.MaterialRaisedButton1.Text = "Tambah User"
         Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
         '
         'username
         '
+        Me.username.BackColor = System.Drawing.Color.White
         Me.username.Depth = 0
         Me.username.Hint = ""
-        Me.username.Location = New System.Drawing.Point(146, 9)
+        Me.username.Location = New System.Drawing.Point(155, 75)
+        Me.username.MaxLength = 32767
         Me.username.MouseState = MaterialSkin.MouseState.HOVER
         Me.username.Name = "username"
         Me.username.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
         Me.username.SelectedText = ""
         Me.username.SelectionLength = 0
         Me.username.SelectionStart = 0
-        Me.username.Size = New System.Drawing.Size(125, 23)
+        Me.username.Size = New System.Drawing.Size(403, 23)
         Me.username.TabIndex = 3
+        Me.username.TabStop = False
         Me.username.UseSystemPasswordChar = False
         '
         'password
         '
+        Me.password.BackColor = System.Drawing.Color.White
         Me.password.Depth = 0
         Me.password.Hint = ""
-        Me.password.Location = New System.Drawing.Point(146, 43)
+        Me.password.Location = New System.Drawing.Point(155, 113)
+        Me.password.MaxLength = 32767
         Me.password.MouseState = MaterialSkin.MouseState.HOVER
         Me.password.Name = "password"
         Me.password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
         Me.password.SelectedText = ""
         Me.password.SelectionLength = 0
         Me.password.SelectionStart = 0
-        Me.password.Size = New System.Drawing.Size(125, 23)
+        Me.password.Size = New System.Drawing.Size(403, 23)
         Me.password.TabIndex = 4
+        Me.password.TabStop = False
         Me.password.UseSystemPasswordChar = False
         '
         'DataGridView1
@@ -104,17 +116,34 @@ Partial Class User
         Me.DataGridView1.AllowUserToAddRows = False
         Me.DataGridView1.AllowUserToDeleteRows = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 130)
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 203)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
         Me.DataGridView1.Size = New System.Drawing.Size(546, 160)
         Me.DataGridView1.TabIndex = 5
         '
+        'MaterialRaisedButton2
+        '
+        Me.MaterialRaisedButton2.AutoSize = True
+        Me.MaterialRaisedButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialRaisedButton2.Depth = 0
+        Me.MaterialRaisedButton2.Icon = Nothing
+        Me.MaterialRaisedButton2.Location = New System.Drawing.Point(16, 152)
+        Me.MaterialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton2.Name = "MaterialRaisedButton2"
+        Me.MaterialRaisedButton2.Primary = True
+        Me.MaterialRaisedButton2.Size = New System.Drawing.Size(56, 36)
+        Me.MaterialRaisedButton2.TabIndex = 6
+        Me.MaterialRaisedButton2.Text = "Back"
+        Me.MaterialRaisedButton2.UseVisualStyleBackColor = True
+        '
         'User
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(570, 302)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(570, 376)
+        Me.Controls.Add(Me.MaterialRaisedButton2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.password)
         Me.Controls.Add(Me.username)
@@ -135,4 +164,5 @@ Partial Class User
     Friend WithEvents username As MaterialSkin.Controls.MaterialSingleLineTextField
     Friend WithEvents password As MaterialSkin.Controls.MaterialSingleLineTextField
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents MaterialRaisedButton2 As MaterialSkin.Controls.MaterialRaisedButton
 End Class
--- Login/Login/User.vb
+++ Login/Login/User.vb
@@ -41,4 +41,14 @@
             isidgv()
         End If
     End Sub
+
+    Private Sub MaterialLabel1_Click(sender As Object, e As EventArgs) Handles MaterialLabel1.Click
+
+    End Sub
+
+    Private Sub MaterialRaisedButton2_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton2.Click
+        Dim SUMenu As New SUMenu
+        SUMenu.Show()
+        Me.Close()
+    End Sub
 End Class
